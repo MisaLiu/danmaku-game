5c75fd2dd3e8cf0a274bba09b1f81d019f15900c
Misa Liu <misaliu@misaliu.top>
2025-07-22 17:49:57 +0800

    feat(client): Multipage test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        stream: resolve(__dirname, 'stream.html'),
+      },
+    },
+  },
 });
